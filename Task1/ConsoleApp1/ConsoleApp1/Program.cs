bffd710b3ab76d247374f5c1cf7e2d365db4dc05
zaminalirustemov <2g17rck@code.edu.az>
2022-11-18 02:16:13 +0400

    Homework

---
+++ Task1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum(Readlinefirst(), Readlinesecond());
+
+        }
+
+        static int Readlinefirst()
+        {
+            int firstNumber = 0;
+            bool validInput = false;
+            while (!validInput)
+            {
+                Console.Write("Please enter your first number: ");
+                var firstNumberInput = Console.ReadLine();
+
+                validInput = int.TryParse(firstNumberInput, out firstNumber);
+
+                if (!validInput)
+                {
+                    Console.WriteLine("Invalid input!");
+
+                }
+            }
+            return firstNumber;
+        }
+        static int Readlinesecond()
+        {
+            int secondNumber = 0;
+            bool validInput = false;
+            while (!validInput)
+            {
+                Console.Write("Please enter your second number: ");
+                var secondNumberInput = Console.ReadLine();
+
+
+                validInput = int.TryParse(secondNumberInput, out secondNumber);
+
+                if (!validInput)
+                {
+                    Console.WriteLine("Invalid input!");
+
+                }
+            }
+            return secondNumber;
+        }
+
+
+
+        static int Sum(int num1, int num2)
+        {
+            int sum = 0;
+            sum = num1 + num2;
+            Console.WriteLine("The sum of these two numbers is=" + sum);
+            return sum;
+        }
+
+
+
+
+    }
+
+}
